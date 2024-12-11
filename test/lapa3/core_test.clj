4751a24f586f02a76d2e7ba5573257eafaec8991
zirtoshka <367222@edu.itmo.ru>
2024-12-11 05:50:25 +0300

    fix linter

--- test/lapa3/core_test.clj
+++ test/lapa3/core_test.clj
@@ -1,21 +1,21 @@
 (ns lapa3.core-test
-  (:require [clojure.test :refer :all]
-            [lapa3.core :refer :all]))
+  (:require [clojure.test :as an-alias]
+            [lapa3.core :refer [my-lagrange-interpolation my-linear-interpolation]]))
 
-(deftest test-linear-interpolation
-  (testing "Линейная интерполяция для двух точек"
+(an-alias/deftest test-linear-interpolation
+  (an-alias/testing "Линейная интерполяция для двух точек"
     (let [points [[0 0] [2 4]]
           step 1
           result (my-linear-interpolation points step)
           expected '([0.0 0] [1.0 2N] [2.0 4])]
-      (is (= result expected)))))
+      (an-alias/is (= result expected)))))
 
-(deftest test-lagrange-interpolation
-  (testing "Лагранжевская интерполяция для нескольких точек"
+(an-alias/deftest test-lagrange-interpolation
+  (an-alias/testing "Лагранжевская интерполяция для нескольких точек"
     (let [points [[0 0] [1 1] [2 4]]
           step 1.0
           start-x 0
           end-x 2
           result (my-lagrange-interpolation points step start-x end-x)
           expected '([0 0] [1.0 1.0] [2.0 4.0])]
-      (is (= result expected)))))
+      (an-alias/is (= result expected)))))
